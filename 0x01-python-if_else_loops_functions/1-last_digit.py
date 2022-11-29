2e2eeff78812c0c5381e36e0297d9ce923b8af7f
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-11-29 15:55:59 +0300

    nested if

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,10 @@ number = random.randint(-10000, 10000)
 number = str(number)
 last = (number[-1])
 last = int(last)
-if last > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5")
+number =int(number)
+if number > 0:
+    if last > 5:
+        print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number} is {last} and is 0")
 elif number < 0:
