05e56a3699255bce8ee347dd402af0d89315e542
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-11-29 15:42:05 +0300

    type-error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,3 +9,5 @@ elif last == "0":
     print(f"Last digit of {number} is {last} and is 0")
 elif last < "6" and last != "0":
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+else:
+    print(f"TypeError")
