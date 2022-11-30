2ac48108187ae19853c5ad1c7088f94170ee4373
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-11-30 15:43:49 +0300

    format strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number >= 0 and number <= 9:
-        print(f"{str(number).zfill(2)}, ", end="")
-    elif number > 9 and number < 99:
-        print(f"{number}, ", end="")
+        print("{:d}".format(number).zfill(2), end=", ")
     else:
-        print(f"{number}")
+        print("{:d}".format(number))
