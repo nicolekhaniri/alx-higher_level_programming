3e14e8885b464476fd57737bde525c8794dadc96
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-11-30 15:52:32 +0300

    your permission

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number >= 0 and number <= 99:
+    if number >= 0 and number <= 98:
         print("{:d}".format(number).zfill(2), end=", ")
     else:
         print("{:d}".format(number))
