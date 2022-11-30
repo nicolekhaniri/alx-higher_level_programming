8d286ebfb4dd2b2f167ebec1374d17e1640c0850
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-11-30 11:28:45 +0300

    with prototype

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    elif i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end=" ")
-    else:
-        print(i, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print(i, end=" ")
+fizzbuzz()
+print("")
