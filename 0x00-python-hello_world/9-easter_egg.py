abdef1ee8c427cea9292f464416bb805ddb21852
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-12-02 13:04:14 +0300

    zen

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,23 +1,3 @@
-is = ("is better than")
-If = ("If the implementation is")
-print(f"The Zen of Python, by Tim Peters
-
-Beautiful {is} ugly.
-Explicit {is} implicit.
-Simple {is} complex.
-Complex {is} complicated.
-Flat {is} nested.
-Sparse {is} dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now {is} never.
-Although never is often better than *right* now.
-{If} hard to explain, it's a bad idea.
-{If} easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!")
+#!/usr/bin/python3
+from zen import zen
+zen()
