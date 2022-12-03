10d5cc39ff1edbdc342f7fe26163ed9a1bc54efb
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-03 08:02:44 -0800

    only if main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv[1:]
-arg_num = len(argv)
-if arg_num  == 0 or arg_num > 1:
-     print(f"{arg_num} arguments")
-elif arg_num  == 1:
-     print(f"{arg_num} argument")
-for i in range(arg_num):
-    print(f"{i+1}: {argv[i]}")
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    arg_num = len(argv)
+    if arg_num  == 0 or arg_num > 1:
+         print(f"{arg_num} arguments")
+    elif arg_num  == 1:
+         print(f"{arg_num} argument")
+    for i in range(arg_num):
+        print(f"{i+1}: {argv[i]}")
