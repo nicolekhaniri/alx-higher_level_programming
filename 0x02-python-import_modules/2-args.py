fb4b29ed20f3bda136901c995f84981f4d45d782
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-03 08:11:08 -0800

    fullstop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     arg_num = len(argv)
-    if arg_num  == 0 or arg_num > 1:
-         print(f"{arg_num} arguments")
+    if arg_num  == 0:
+        print(f"{arg_num} arguments.")
     elif arg_num  == 1:
-         print(f"{arg_num} argument")
+        print(f"{arg_num} argument:")
+    elif arg_num == > 1:
+        print(f"{arg_num} arguments:")
     for i in range(arg_num):
         print(f"{i+1}: {argv[i]}")
