56f6020266caed80f5a94ab5f5922da8afc4077e
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-03 08:29:41 -0800

    pycode style checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(arg_num))
     elif arg_num == 1:
         print("{} argument:".format(arg_num))
-    elif arg_num  > 1:
+    elif arg_num > 1:
         print("{} arguments:".format(arg_num))
     for i in range(arg_num):
         print("{}: {}".format(i+1, argv[i]))
