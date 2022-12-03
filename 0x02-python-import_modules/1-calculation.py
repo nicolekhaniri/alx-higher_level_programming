8139d62787c164ea04a014390d74ae2d126c7942
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-03 09:08:41 -0800

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    if add():
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif sub():
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif mul():
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif div():
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+     print(f"{a + b}")
+     return (a + b)
+
+def sub(a, b):
+     print(f"{a - b}")
+     return (a - b)
+
+def mul(a, b):
+     print(f"{a * b}")
+     return (a * b)
+
+def div(a, b):
+    print(f"{a / b}")
+    return (a / b)
