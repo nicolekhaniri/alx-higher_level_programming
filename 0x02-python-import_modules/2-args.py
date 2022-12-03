e8c206c4902b6cf633a5bb86877eae09ed4d3ff3
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-03 08:20:38 -0800

    pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import sys
+
 if __name__ == "__main__":
+    import sys
+
     argv = sys.argv[1:]
     arg_num = len(argv)
-    if arg_num  == 0:
+
+    if arg_num == 0:
         print(f"{arg_num} arguments.")
-    elif arg_num  == 1:
+    elif arg_num == 1:
         print(f"{arg_num} argument:")
     elif arg_num == > 1:
         print(f"{arg_num} arguments:")
