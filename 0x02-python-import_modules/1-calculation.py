27fc35f44b2065cfd7e4ada7e612800b2b8b2701
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-12-03 15:52:41 +0300

    include subtract

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
     if add():
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sub():
+        print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
     elif mul():
         print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
     elif div():
