88ab153f1776a5da8c3a54708729a0bc0e3d19ad
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-09 04:47:15 -0800

    error on 10-main.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-new_list = my_list.copy()
-for i in my_list:
-    if i % 2 == 0:
-        print(f"{i} is divisible by 2")
-    else:
-        print(f"{i} is not divisible by 2")
+    for i in my_list:
+        if i % 2 == 0:
+            print(f"{i} is divisible by 2")
+        else:
+            print(f"{i} is not divisible by 2")
