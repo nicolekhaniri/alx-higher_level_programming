28f5232b79254c5065cdd61eb759a91e65c5f3a1
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-09 05:07:58 -0800

    initializing the position of the maximum value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list[]:
-        return None
+    if my_list == " ":
+        return(None)
     else:
+        max_value = my_list[0]
         for i in my_list:
-            if i > i++:
-                i = max_value
+            if max_value < i:
+                max_value = i
                 return max_value
