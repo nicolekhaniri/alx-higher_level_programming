f540b7728dcc1ef6d50c8c07883c84fcd541faea
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-09 05:16:40 -0800

    creating a new list instead of overriding the old one

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    my_list2 = []
     for i in my_list:
         if i % 2 == 0:
-            my_list.append(True)
+            my_list2.append(True)
         else:
-            my_list.append(False)
-    return my_list
+            my_list2.append(False)
+    return my_list2
