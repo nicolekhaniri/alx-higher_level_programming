659fbc49c17a01f15e3dac4881f8c757be080de4
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-09 13:21:33 -0800

    nesting for loop to return the new list instead of the iterator

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,6 @@
 def common_elements(set_1, set_2):
     set_3 = []
     for i in set_1:
-        for j in set_2:
-            if i == j:
-                set_3.append(i)
-    return i
+        if i in set_2:
+            set_3.append(i)
+    return set_3
