962d888b752c18498954e766f2d9592ff06d36e7
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-09 13:13:22 -0800

    creating an empty list

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    set_3 = []
     for i in set(set_1):
         for j in set(set_2):
             if i == j:
+                set_3.append(i)
                 return i
