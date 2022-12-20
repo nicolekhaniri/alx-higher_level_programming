351a0431e3719200ec4927dd77b3703765842ca1
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-20 13:33:20 -0800

    pycodestyle for 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+""" class square """
+
+
 class Square:
     """ defines the class square """
     def __init__(self, size=0):
-        """ creates the private instance attribute size """
-        """ initializes size as zero """
+        """ creates the private instance attribute size
+        Args: size is initialized as size zero
+        """
         if size != type(int):
             raise TypeError("size must be an integer")
         elif size < 0:
