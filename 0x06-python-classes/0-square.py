bdf596b52cf2388d25056cd34d5351028757293e
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-20 13:21:45 -0800

    documantation for 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Define a square """
+
+
 class Square:
-    '0-square.Square'
+    def __init__(self, size):
+        """ The square class
+        Args: size - representing the squares size
+        """
+
+        self.__size = size
