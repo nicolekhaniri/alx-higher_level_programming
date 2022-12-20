b5437aedd834f69119eda3fe4e7d4b12d5a57d1a
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-20 13:30:54 -0800

    pycodestyle for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+""" defines a square """
+
+
 class Square:
     """ class Square to define a square based on 0-square.py """
-    """ size is a private attribute """ 
+    """ size is a private attribute """
     def __init__(self, size=0):
-        """ size is initialized as zero """
+        """ size is initialized as zero
+        Args: size - represents the size of the square
+        """
         self.__size = size
