9a9b3bfa9519e4c77ced4d58da2663adb593777d
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-20 13:44:48 -0800

    fixing the pycode and adding the call

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+"""area of a square """
+
+
 class Square:
     """ define class Square """
-    def __init__(self, size = 0):
-        """ creates private instance variable size """
-        """ initializes size to 0 """
+
+    def __init__(self, size=0):
+        """ creates private instance variable size
+        Args: size - is initialized to size to 0
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
             """ size has to be an integer """
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
+        """calculate and return the area of the square"""
         return self.__size ** 2
