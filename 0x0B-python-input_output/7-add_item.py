1163dd53c8f07fabbdcf0917e1d63335d15b555a
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-11 00:46:37 +0300

    executing it form the main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,11 @@ import sys
 from 5-save_to_json_file import *
 from 6-load_from_json_file import *
 
-filename = "add_item.json"
-try:
-    existing_file = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_file = []
-new_file = existing_file + argv[:1]
-save_to_json_file(new_file, filename)
+if __name__ = "__main__":
+    filename = "add_item.json"
+    try:
+        existing_file = load_from_json_file(filename)
+    except FileNotFoundError:
+        existing_file = []
+    new_file = existing_file + argv[:1]
+    save_to_json_file(new_file, filename)
