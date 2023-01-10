db2dc3d43d1da7b78b20a203582e42c8e4ca1f58
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-10 19:45:31 +0300

    utf fix by adding hyphen

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, w, encoding="utf8") as file:
+    with open(filename, w, encoding="utf-8") as file:
         newfile = file.write(text)
         return newfile
