3a5e68daa8fea1b8c01049d880489bdb7c1c231e
nicolekhaniri <nicolekhaniri@gmail.com>
2023-01-10 12:19:09 -0800

    adding an end to the print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename(str): The name of the file that will be read
     """
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
