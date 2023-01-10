e9a1fb61dbbedb3eb7721bac657853013e8dcce4
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-11 00:39:01 +0300

    adding args documentation and fixing the write command

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,6 +11,8 @@ import json
 def load_from_json_file(filename):
     """
     creates object from json file
+    Args:
+        filename: file
     """
-    with open(filename) as new:
+    with open(filename, 'w', encoding="utf-8") as new:
         json.load(new)
