d0f8554d455c8cb80ae3cefd81ecec94782f8416
nicolekhaniri <nicolekhaniri@gmail.com>
2023-01-11 06:27:37 -0800

    shortening code and adding documentation for return

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,9 @@
 
 
 def inherits_from(obj, a_class):
-    if type(obj) is not a_class and issubclass(type(obj), a_class):
-        return True
-    else:
-        return False
+
+    """
+    Returns true only if is an inheritance
+    """
+
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
