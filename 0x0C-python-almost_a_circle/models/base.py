7da074856417be79894d327b951cef25e6c57364
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-23 16:38:11 +0300

    line 20 fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
             id = __nb_objects
+            __nb_objects += 1
             self.id = id
 
     @staticmethod
